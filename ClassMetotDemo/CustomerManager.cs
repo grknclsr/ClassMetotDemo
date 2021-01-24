f284f79762245d2c4745b55e7467b76b332b0102
GURKAN CALISIR <gurkan01c@gmail.com>
2021-01-24 21:50:54 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Güncellendi");
+        }
+        public void List()
+        {
+            Console.WriteLine("Listelendi");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 01;
+            customer.Name = "Gürkan";
+            customer.Surname = "Çalışır";
+            customer.Age = 20;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 02;
+            customer2.Name = "Ahmet";
+            customer2.Surname = "Sarı";
+            customer2.Age = 21;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 03;
+            customer3.Name = "Hikmet";
+            customer3.Surname = "Köfte";
+            customer3.Age = 22;
+
+            Customer[] customers = new Customer[] { customer2, customer, customer3 };
+
+
+            foreach (var custom in customers)
+            {
+                Console.WriteLine("Id: " + custom.Id + "\n Name: " + custom.Name + "\n Surname: " + custom.Surname + "\n Age: " + custom.Age);
+                Console.WriteLine("**********************************");
+
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Delete();
+            customerManager.Update();
+            customerManager.List();
+
+
+        }
+    }
+}
